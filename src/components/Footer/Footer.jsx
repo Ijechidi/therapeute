b7bbb19dfec939351792a97d11e56fa60a65d910
Ijechidi <chidinma.ijeom@gmail.com>
2025-04-12 16:11:26 +0000

    update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+
+
 
 const Footer = () => {
   return (
